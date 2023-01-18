adb3d7bc0e7f9216694b4c837a19b976e5c66232
Liideli <roope.kasperi.laine@gmail.com>
2023-01-18 09:31:30 +0200

    Small fix

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -28,4 +28,18 @@ const useMedia = () => {
   return {mediaArray};
 };
 
-export {useMedia};
+const useAuthentication = () => {
+  const postLogin = async () => {
+    // TODO: post login to api
+    // https://media.mw.metropolia.fi/wbma/docs/#api-Authentication-PostAuth
+  };
+};
+
+const useUser = () => {
+  const checkUser = async () => {
+    // call https://media.mw.metropolia.fi/wbma/docs/#api-User-CheckUserName
+  };
+  // https://media.mw.metropolia.fi/wbma/docs/#api-User
+};
+
+export {useMedia, useAuthentication, useUser};
